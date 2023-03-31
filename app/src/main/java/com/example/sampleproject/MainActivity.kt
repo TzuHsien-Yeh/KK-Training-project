5d2e68ff51e8d03ad1bcd625f843a6eb5af39685
tzuhsienyeh <tzuhsienyeh@kkcompany.com>
2023-03-31 18:58:21 +0800

    Set nav drawer and bottom nav view

--- app/src/main/java/com/example/sampleproject/MainActivity.kt
+++ app/src/main/java/com/example/sampleproject/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.sampleproject
 
 import android.os.Bundle
-import android.view.View
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.sampleproject.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,6 +20,8 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,17 +40,66 @@ class MainActivity : AppCompatActivity() {
             when (destination.id) {
                 R.id.listFragment -> {
                     toolbar.setTitle(R.string.attraction_list_page_title)
-                    binding.btnFav.visibility = View.VISIBLE
-                    binding.btnFav.setOnClickListener {
-                        controller.navigate(NavGraphDirections.actionGlobalMyFavoriteFragment())
-                    }
                 }
-                R.id.detailFragment -> {
-                    binding.btnFav.visibility = View.GONE
-                }
+                R.id.detailFragment -> {}
                 R.id.myFavoriteFragment -> {
                     toolbar.setTitle(R.string.my_favorites)
-                    binding.btnFav.visibility = View.GONE
+                }
+            }
+        }
+
+        setUpBottomNav()
+        setUpDrawer()
+    }
+
+    private fun setUpDrawer() {
+        toggle = ActionBarDrawerToggle(this, binding.drawerLayout, R.string.open, R.string.close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navView: NavigationView = binding.drawerNavView
+
+        navView.setNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.listFragment -> {
+                    if (navController.currentDestination?.id != R.id.listFragment) {
+                        navController.navigate(NavGraphDirections.actionGlobalListFragment())
+                        binding.drawerLayout.closeDrawers()
+                    }
+                }
+                R.id.myFavoriteFragment -> {
+                    if (navController.currentDestination?.id != R.id.myFavoriteFragment) {
+                        navController.navigate(NavGraphDirections.actionGlobalMyFavoriteFragment())
+                        binding.drawerLayout.closeDrawers()
+                    }
+                }
+            }
+            true
+        }
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setUpBottomNav() {
+        binding.bottomNavView.setOnItemReselectedListener { item ->
+            when(item.itemId) {
+                R.id.listFragment -> {
+                    if (navController.currentDestination?.id != R.id.listFragment) {
+                        navController.navigate(NavGraphDirections.actionGlobalListFragment())
+                    }
+                }
+
+                R.id.myFavoriteFragment -> {
+                    if (navController.currentDestination?.id != R.id.myFavoriteFragment) {
+                        navController.navigate(NavGraphDirections.actionGlobalMyFavoriteFragment())
+                    }
                 }
             }
         }
